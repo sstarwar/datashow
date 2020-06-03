94161f6d368b6d8132b9365b2ea8e9cf63e6da24
赵聪聪 <zhaochenwei@zhaocongcongs-MacBook-Pro.local>
2020-06-03 21:46:56 +0800

    init my project

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <!-- header -->
+  <mt-header title="标题过长会隐藏后面的内容啊哈哈哈哈">
+   <router-link to="/" slot="left">
+    <mt-button icon="back">返回</mt-button>
+   </router-link>
+  <mt-button icon="more" slot="right"></mt-button>
+  </mt-header>
+    <!-- router-view -->
+    <!-- tabbar -->
+  <nav class="mui-bar mui-bar-tab">
+   <a class="mui-tab-item mui-active" href="#tabbar">
+    <span class="mui-icon mui-icon-home"></span>
+    <span class="mui-tab-label">首页</span>
+   </a>
+   <a class="mui-tab-item" href="#tabbar-with-chat">
+    <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+    <span class="mui-tab-label">消息</span>
+   </a>
+   <a class="mui-tab-item" href="#tabbar-with-contact">
+    <span class="mui-icon mui-icon-contact"></span>
+    <span class="mui-tab-label">通讯录</span>
+   </a>
+   <a class="mui-tab-item" href="#tabbar-with-map">
+    <span class="mui-icon mui-icon-gear"></span>
+    <span class="mui-tab-label">设置</span>
+   </a>
+  </nav>
+   <h1>123</h1>
+
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App'
 }
+</script>
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+<style scoped>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import './lib/mui/css/mui.min.css'
+import { Header, Button } from 'mint-ui'
+Vue.component(Header.name, Header)
+Vue.component(Button.name, Button)
 
 Vue.config.productionTip = false
 
